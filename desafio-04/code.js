b233dc5b863a08df701b73ea25e662e13874c4aa
Armano <armanobarrosalvesh@gmail.com>
2023-01-31 19:48:52 -0300

    Arquivos do desafio 04 da Aula 01

--- desafio-04/code.js
+++ desafio-04/code.js
@@ -1,7 +1,22 @@
-var valorEmdolar = 64;
+var nomeUsuario = document.querySelector(".nomeUsuario");
 var cotacaoDoDolar = 5.32;
+var cotacaoDoEuro = 5.54;
+var cotacaoDoIene = 0.039;
+var cotacaoDoBitC = 117260.42;
 
-var valorEmReal = valorEmdolar * cotacaoDoDolar;
-valorEmReal = valorEmReal.toFixed(2);
+var valorEmDolar = parseFloat(prompt("Digite um valor em dolares para converter em real: "))
+var valorEmEuro = parseFloat(prompt("Digite um valor em euros para converter em real: "))
+var valorEmIene = parseFloat(prompt("Digite um valor em ienes para converter em real: "))
+var valorEmBitC = parseFloat(prompt("Digite um valor em BitCoin para converter em real: "))
 
-alert('R$ ' + valorEmReal);
+var valorEmReal1 = valorEmDolar * cotacaoDoDolar;
+var valorEmReal2 = valorEmEuro * cotacaoDoEuro;
+var valorEmReal3 = valorEmIene * cotacaoDoIene;
+var valorEmReal4 = valorEmBitC * cotacaoDoBitC;
+
+valorEmReal1 = valorEmReal1.toFixed(2);
+valorEmReal2 = valorEmReal2.toFixed(2);
+valorEmReal3 = valorEmReal3.toFixed(2);
+valorEmReal4 = valorEmReal4.toFixed(2);
+
+alert('$ ' + valorEmDolar +' dolares equilave a R$ ' + valorEmReal1 + ' reais, € ' +  valorEmEuro + ' euros equivale a R$ ' + valorEmReal2 + ' reais, ¥ ' + valorEmIene + ' ienes equivale a R$ ' + valorEmReal3 + ' reais, BTC ' + valorEmBitC + 'BitCoins equivale a R$ ' + valorEmReal4 + ' reais.');
